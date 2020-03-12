503e190acad636c7709e384b57e56aaca1fe01a7
di39258 <tobias.bodo@dillinger.biz>
2020-03-12 15:25:14 +0100

    Hinzufuegen der pom.xml , java Klasse und der testklasse

---
+++ src/test/java/de/dillinger/stw/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package de.dillinger.stw;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest
+{
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
