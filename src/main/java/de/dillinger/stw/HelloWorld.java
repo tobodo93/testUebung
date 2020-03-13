f23e0aa7532ee7427dc799bf22d6f84639424e0f
Tobias Bodo <55879405+tobodo93@users.noreply.github.com>
2020-03-13 08:17:54 +0100

    Abänderung der helloworld klasse

--- src/main/java/de/dillinger/stw/HelloWorld.java
+++ src/main/java/de/dillinger/stw/HelloWorld.java
@@ -4,7 +4,7 @@ package de.dillinger.stw;
 public class HelloWorld
 {
     public static void main(String[]args){
-        System.out.println("Hello World!");
+        System.out.println("Halloo World!");
     }
 }
 
